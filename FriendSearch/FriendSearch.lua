d95befe3a8fbf94996c8e3e31a78250943f93612
abuck199 <alex.footbal@gmail.com>
2023-01-01 13:19:08 -0500

    initial commit of FriendSearchWoW Repo

---
+++ FriendSearch/FriendSearch.lua
@@ -0,0 +1,426 @@
+colorsRGB = {
+	aliceblue = {240, 248, 255},
+	antiquewhite = {250, 235, 215},
+	aqua = { 0, 255, 255},
+	aquamarine = {127, 255, 212},
+	azure = {240, 255, 255},
+	beige = {245, 245, 220},
+	bisque = {255, 228, 196},
+	black = { 0, 0, 0},
+	blanchedalmond = {255, 235, 205},
+	blue = { 0, 0, 255},
+	blueviolet = {138, 43, 226},
+	brown = {165, 42, 42},
+	burlywood = {222, 184, 135},
+	cadetblue = { 95, 158, 160},
+	chartreuse = {127, 255, 0},
+	chocolate = {210, 105, 30},
+	coral = {255, 127, 80},
+	cornflowerblue = {100, 149, 237},
+	cornsilk = {255, 248, 220},
+	crimson = {220, 20, 60},
+	cyan = { 0, 255, 255},
+	darkblue = { 0, 0, 139},
+	darkcyan = { 0, 139, 139},
+	darkgoldenrod = {184, 134, 11},
+	darkgray = {169, 169, 169},
+	darkgreen = { 0, 100, 0},
+	darkgrey = {169, 169, 169},
+	darkkhaki = {189, 183, 107},
+	darkmagenta = {139, 0, 139},
+	darkolivegreen = { 85, 107, 47},
+	darkorange = {255, 140, 0},
+	darkorchid = {153, 50, 204},
+	darkred = {139, 0, 0},
+	darksalmon = {233, 150, 122},
+	darkseagreen = {143, 188, 143},
+	darkslateblue = { 72, 61, 139},
+	darkslategray = { 47, 79, 79},
+	darkslategrey = { 47, 79, 79},
+	darkturquoise = { 0, 206, 209},
+	darkviolet = {148, 0, 211},
+	deeppink = {255, 20, 147},
+	deepskyblue = { 0, 191, 255},
+	dimgray = {105, 105, 105},
+	dimgrey = {105, 105, 105},
+	dodgerblue = { 30, 144, 255},
+	firebrick = {178, 34, 34},
+	floralwhite = {255, 250, 240},
+	forestgreen = { 34, 139, 34},
+	fuchsia = {255, 0, 255},
+	gainsboro = {220, 220, 220},
+	ghostwhite = {248, 248, 255},
+	gold = {255, 215, 0},
+	goldenrod = {218, 165, 32},
+	gray = {128, 128, 128},
+	grey = {128, 128, 128},
+	green = { 0, 128, 0},
+	greenyellow = {173, 255, 47},
+	honeydew = {240, 255, 240},
+	hotpink = {255, 105, 180},
+	indianred = {205, 92, 92},
+	indigo = { 75, 0, 130},
+	ivory = {255, 255, 240},
+	khaki = {240, 230, 140},
+	lavender = {230, 230, 250},
+	lavenderblush = {255, 240, 245},
+	lawngreen = {124, 252, 0},
+	lemonchiffon = {255, 250, 205},
+	lightblue = {173, 216, 230},
+	lightcoral = {240, 128, 128},
+	lightcyan = {224, 255, 255},
+	lightgoldenrodyellow = {250, 250, 210},
+	lightgray = {211, 211, 211},
+	lightgreen = {144, 238, 144},
+	lightgrey = {211, 211, 211},
+	lightpink = {255, 182, 193},
+	lightsalmon = {255, 160, 122},
+	lightseagreen = { 32, 178, 170},
+	lightskyblue = {135, 206, 250},
+	lightslategray = {119, 136, 153},
+	lightslategrey = {119, 136, 153},
+	lightsteelblue = {176, 196, 222},
+	lightyellow = {255, 255, 224},
+	lime = { 0, 255, 0},
+	limegreen = { 50, 205, 50},
+	linen = {250, 240, 230},
+	magenta = {255, 0, 255},
+	maroon = {128, 0, 0},
+	mediumaquamarine = {102, 205, 170},
+	mediumblue = { 0, 0, 205},
+	mediumorchid = {186, 85, 211},
+	mediumpurple = {147, 112, 219},
+	mediumseagreen = { 60, 179, 113},
+	mediumslateblue = {123, 104, 238},
+	mediumspringgreen = { 0, 250, 154},
+	mediumturquoise = { 72, 209, 204},
+	mediumvioletred = {199, 21, 133},
+	midnightblue = { 25, 25, 112},
+	mintcream = {245, 255, 250},
+	mistyrose = {255, 228, 225},
+	moccasin = {255, 228, 181},
+	navajowhite = {255, 222, 173},
+	navy = { 0, 0, 128},
+	oldlace = {253, 245, 230},
+	olive = {128, 128, 0},
+	olivedrab = {107, 142, 35},
+	orange = {255, 165, 0},
+	orangered = {255, 69, 0},
+	orchid = {218, 112, 214},
+	palegoldenrod = {238, 232, 170},
+	palegreen = {152, 251, 152},
+	paleturquoise = {175, 238, 238},
+	palevioletred = {219, 112, 147},
+	papayawhip = {255, 239, 213},
+	peachpuff = {255, 218, 185},
+	peru = {205, 133, 63},
+	pink = {255, 192, 203},
+	plum = {221, 160, 221},
+	powderblue = {176, 224, 230},
+	purple = {128, 0, 128},
+	red = {255, 0, 0},
+	rosybrown = {188, 143, 143},
+	royalblue = { 65, 105, 225},
+	saddlebrown = {139, 69, 19},
+	salmon = {250, 128, 114},
+	sandybrown = {244, 164, 96},
+	seagreen = { 46, 139, 87},
+	seashell = {255, 245, 238},
+	sienna = {160, 82, 45},
+	silver = {192, 192, 192},
+	skyblue = {135, 206, 235},
+	slateblue = {106, 90, 205},
+	slategray = {112, 128, 144},
+	slategrey = {112, 128, 144},
+	snow = {255, 250, 250},
+	springgreen = { 0, 255, 127},
+	steelblue = { 70, 130, 180},
+	tan = {210, 180, 140},
+	teal = { 0, 128, 128},
+	thistle = {216, 191, 216},
+	tomato = {255, 99, 71},
+	turquoise = { 64, 224, 208},
+	violet = {238, 130, 238},
+	wheat = {245, 222, 179},
+	white = {255, 255, 255},
+	whitesmoke = {245, 245, 245},
+	yellow = {255, 255, 0},
+	yellowgreen = {154, 205, 50}
+}
+colorsRGB.R = function (name)
+	return colorsRGB[name][1] / 255
+end
+
+colorsRGB.G = function (name)
+	return colorsRGB[name][2] / 255
+end
+
+colorsRGB.B = function (name)
+	return colorsRGB[name][3] / 255
+end
+
+colorsRGB.RGB = function (name)
+	return colorsRGB[name][1],colorsRGB[name][2],colorsRGB[name][3]
+end
+
+function tprint (tbl, indent)
+	if not indent then indent = 0 end
+	local toprint = string.rep(" ", indent) .. "{\r\n"
+	indent = indent + 2 
+	for k, v in pairs(tbl) do
+	  toprint = toprint .. string.rep(" ", indent)
+	  if (type(k) == "number") then
+		toprint = toprint .. "[" .. k .. "] = "
+	  elseif (type(k) == "string") then
+		toprint = toprint  .. k ..  "= "   
+	  end
+	  if (type(v) == "number") then
+		toprint = toprint .. v .. ",\r\n"
+	  elseif (type(v) == "string") then
+		toprint = toprint .. "\"" .. v .. "\",\r\n"
+	  elseif (type(v) == "table") then
+		toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+	  else
+		toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+	  end
+	end
+	toprint = toprint .. string.rep(" ", indent-2) .. "}"
+	return toprint
+end
+--------------------------------------------------------------------------------
+-- Show WoWOnline/Total friend 
+--------------------------------------------------------------------------------
+local FriendOnline = CreateFrame("Frame","FriendOnline", FriendsTabHeaderTab3, "BackdropTemplate")
+FriendOnline:SetWidth(22)
+FriendOnline:SetHeight(22)
+FriendOnline:SetPoint("RIGHT", 21, -5)
+FriendOnline:Show()
+
+FriendOnline.icon = FriendOnline:CreateTexture("FriendOnlineIcon", "BACKGROUND")
+FriendOnline.icon:SetAllPoints()
+FriendOnline.icon:SetSize(32,32)
+FriendOnline.icon:SetAtlas("Battlenet-ClientIcon-WoW", true)
+
+FriendOnline.text = FriendOnline:CreateFontString(nil,"OVERLAY","GameTooltipText")
+FriendOnline.text:ClearAllPoints()
+FriendOnline.text:SetPoint("RIGHT",FriendOnline.icon,"RIGHT",46,0)
+FriendOnline.text.SetPoint = function() end
+
+function getTotalWowFriendOnline()
+	indexlocalOnline = 0
+	for i = 1, BNGetNumFriends() do
+		for j = 1, C_BattleNet.GetFriendNumGameAccounts(i) do
+			local game = C_BattleNet.GetFriendGameAccountInfo(i, j)
+			if game.characterName ~= nil then
+				for k, v in pairs{game.characterName} do
+					indexlocalOnline = indexlocalOnline + 1
+				end
+			end
+		end
+	end
+	FriendsFrameTitleText:SetText("You currently have "..indexlocalOnline.. " playing WoW")
+	FriendsFrameTitleText:SetVertexColor(colorsRGB.R("limegreen"), colorsRGB.G("limegreen"), colorsRGB.B("limegreen"), 1)
+
+end
+getTotalWowFriendOnline()
+
+function UpdateOnlineFriend()
+	numFriends, numOnline = BNGetNumFriends()
+	FriendOnline.text:SetPoint("CENTER")
+	FriendOnline.text:SetText(indexlocalOnline.."/"..numFriends)
+end
+
+hooksecurefunc("FriendsList_Update", UpdateOnlineFriend)
+hooksecurefunc("FriendsList_Update", getTotalWowFriendOnline)
+
+--------------------------------------------------------------------------------
+-- Horde/Alliance Filter
+--------------------------------------------------------------------------------
+-- function FriendApplyFilter()
+-- 	for i = 1, BNGetNumFriends() do
+-- 		for j = 1, C_BattleNet.GetFriendNumGameAccounts(i) do
+-- 			local game = C_BattleNet.GetFriendGameAccountInfo(i, j)
+-- 			if game.characterName ~= nil then
+-- 				-- if game.factionName then
+-- 				-- 	FriendsListFrame.ScrollBox.ScrollTarget.background:SetAlpha(0)
+-- 				-- end
+-- 				print(game.characterName, game.factionName)
+-- 			end	
+-- 		end
+-- 	end
+-- end
+
+-- FriendApplyFilter()
+--------------------------------------------------------------------------------
+-- Search Filter
+--------------------------------------------------------------------------------
+FriendsFrameAddFriendButton:SetWidth(90)
+FriendsFrameSendMessageButton:Hide()
+
+FriendSearchBar = CreateFrame("EditBox", "FriendSearchBar", FriendsListFrame,"InputBoxTemplate")
+FriendSearchBar:SetWidth(230)
+FriendSearchBar:SetHeight(21)
+FriendSearchBar:SetPoint("BOTTOMRIGHT", -7, 4)
+FriendSearchBar:Show()
+FriendSearchBar:SetAutoFocus(false)
+FriendSearchBar:ClearFocus()
+
+FriendSearchBar:SetScript("OnTextChanged", function()
+	UpdateFriendButtons()
+end )
+
+-- function UpdateFriendButtons()
+-- 	UpdateFriendInfo()
+-- 	-- local FriendsFrameFriendButtons = FriendsFrameFriendsScrollFrame.buttons -- 8.0.1
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrameScrollFrame.buttons -- 8.3.7
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBox:ForEachFrame(function(button) end) -- 10.0.2
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.Track.Thumb
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.buttons
+
+-- 	for i = 1, #FriendsFrameFriendButtons do
+
+-- 		local button = FriendsFrameFriendButtons[i]
+
+-- 		local FriendSearchLiteSearchText = FriendSearchBar:GetAttribute(string.match(button:GetText(),"%d+"))
+
+-- 		if FriendSearchLiteSearchText ~= nil then
+
+-- 			local FriendSearchLiteSearchTextBnet = FriendSearchLiteSearchText[1]
+-- 			local FriendSearchLiteSearchTextChar = FriendSearchLiteSearchText[2]
+-- 			local FriendSearchLiteSearchTextTotal = ""
+-- 			if FriendSearchLiteSearchTextBnet ~= nil then
+-- 				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal..FriendSearchLiteSearchTextBnet
+-- 			end
+-- 			if FriendSearchLiteSearchTextChar ~= nil then
+-- 				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal.."+"..FriendSearchLiteSearchTextChar
+-- 			end
+-- 			if FriendSearchBar:GetText() == "" then
+-- 				button:SetAlpha(1)
+-- 			elseif string.find(string.lower(FriendSearchLiteSearchTextTotal),string.lower(FriendSearchBar:GetText())) == nil then
+-- 				button:SetAlpha(.25)
+-- 			else
+-- 				button:SetAlpha(1)
+-- 			end
+-- 		end
+-- 	end
+-- end
+
+function UpdateFriendButtons()
+	UpdateFriendInfo()
+
+	-- local FriendsFrameFriendButtons = FriendsFrameFriendsScrollFrame.buttons -- 8.0.1
+	-- local FriendsFrameFriendButtons = FriendsListFrameScrollFrame.buttons -- 8.3.7
+	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBox:ForEachFrame(function(button) end) -- 10.0.2
+	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar
+	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.Track.Thumb
+	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.buttons
+	
+	FriendsListFrame.ScrollBox:ForEachFrame(function(button)
+		if string.find(string.lower(FriendSearchBar:GetText()), string.lower(UpdateFriendInfo()[1])) or string.find(string.lower(FriendSearchBar:GetText()), string.lower(UpdateFriendInfo()[2])) then
+		  print ("The friend " .. string.lower(UpdateFriendInfo()[2]) .." WAS FOUND.")
+		  button:Hide()
+		else
+		  print ("The word WAS NOT FOUND.")
+		  button:Show()
+		end
+		local FriendSearchLiteSearchText = FriendSearchBar:GetAttribute(string.match(button:GetText(),"%d+"))
+		
+		if FriendSearchLiteSearchText ~= nil then
+			local FriendSearchLiteSearchTextBnet = FriendSearchLiteSearchText[1]
+			local FriendSearchLiteSearchTextChar = FriendSearchLiteSearchText[2]
+			local FriendSearchLiteSearchTextTotal = ""
+			if FriendSearchLiteSearchTextBnet ~= nil then
+				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal..FriendSearchLiteSearchTextBnet
+			end
+			if FriendSearchLiteSearchTextChar ~= nil then
+				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal.."+"..FriendSearchLiteSearchTextChar
+			end
+			if FriendSearchBar:GetText() == "" then
+				button:SetAlpha(1)
+			elseif string.find(string.lower(FriendSearchLiteSearchTextTotal),string.lower(FriendSearchBar:GetText())) == nil then
+				button:SetAlpha(.25)
+			else
+				button:SetAlpha(1)
+			end
+		end
+	end);
+end
+
+-- function UpdateFriendButtons()
+-- 	UpdateFriendInfo()
+-- 	-- local FriendsFrameFriendButtons = FriendsFrameFriendsScrollFrame.buttons -- 8.0.1
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrameScrollFrame.buttons -- 8.3.7
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBox:ForEachFrame(function(button) end) -- 10.0.2
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.Track.Thumb
+-- 	-- local FriendsFrameFriendButtons = FriendsListFrame.ScrollBar.buttons
+	
+-- 	FriendsListFrame.ScrollBox:ForEachFrame(function(button)
+-- 		local FriendSearchLiteSearchText = FriendSearchBar:GetAttribute(string.match(button:GetText(),"%d+"))
+		
+-- 		if FriendSearchLiteSearchText ~= nil then
+-- 			local FriendSearchLiteSearchTextBnet = FriendSearchLiteSearchText[1]
+-- 			local FriendSearchLiteSearchTextChar = FriendSearchLiteSearchText[2]
+-- 			local FriendSearchLiteSearchTextTotal = ""
+-- 			if FriendSearchLiteSearchTextBnet ~= nil then
+-- 				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal..FriendSearchLiteSearchTextBnet
+-- 			end
+-- 			if FriendSearchLiteSearchTextChar ~= nil then
+-- 				FriendSearchLiteSearchTextTotal = FriendSearchLiteSearchTextTotal.."+"..FriendSearchLiteSearchTextChar
+-- 			end
+-- 			if FriendSearchBar:GetText() == "" then
+-- 				button:SetAlpha(1)
+-- 			elseif string.find(string.lower(FriendSearchLiteSearchTextTotal),string.lower(FriendSearchBar:GetText())) == nil then
+-- 				button:SetAlpha(.25)
+-- 			else
+-- 				button:SetAlpha(1)
+-- 			end
+-- 		end
+-- 	end);
+-- end
+
+
+local FriendSearchLiteOriginalOnMouseWheel = FriendsListFrame.ScrollBox:GetScript("OnMouseWheel")
+
+FriendsListFrame.ScrollBox:SetScript("OnMouseWheel", function(...)
+	FriendSearchLiteOriginalOnMouseWheel(...)
+	UpdateFriendButtons()
+end )
+
+FriendSearchBar:SetScript("OnEnterPressed", function()
+	FriendSearchBar:SetAutoFocus(false)
+	FriendSearchBar:ClearFocus()
+	UpdateFriendButtons()
+end )
+
+FriendSearchBar:SetScript("OnHide", function()
+	FriendSearchBar:SetText("")
+	FriendSearchBar:SetAutoFocus(false)
+	FriendSearchBar:ClearFocus()
+end )
+
+function UpdateFriendInfo()
+	numFriends, numOnline = BNGetNumFriends()
+	for i=1, numFriends do
+		local bnetIDAccount, accountName, battleTag, isBattleTagPresence, characterName, bnetIDGameAccount, client, isOnline, lastOnline, isAFK, isDND, messageText, noteText, isRIDFriend, messageTime, canSoR, isReferAFriend, canSummonFriend = BNGetFriendInfo(i)
+		return FriendSearchBar:SetAttribute(string.match(accountName,"%d+"),{battleTag,characterName})
+		-- return {battleTag, characterName}
+	end
+end
+
+local function getDeprecatedAccountInfo(accountInfo)
+	if accountInfo then
+		local wowProjectID = accountInfo.gameAccountInfo.wowProjectID or 0;
+		local clientProgram = accountInfo.gameAccountInfo.clientProgram ~= "" and accountInfo.gameAccountInfo.clientProgram or nil;
+		return	accountInfo.bnetAccountID, accountInfo.accountName, accountInfo.battleTag, accountInfo.isBattleTagFriend,
+				accountInfo.gameAccountInfo.characterName, accountInfo.gameAccountInfo.gameAccountID, clientProgram,
+				accountInfo.gameAccountInfo.isOnline, accountInfo.lastOnlineTime, accountInfo.isAFK, accountInfo.isDND, accountInfo.customMessage, accountInfo.note, accountInfo.isFriend,
+				accountInfo.customMessageTime, wowProjectID, accountInfo.rafLinkType == Enum.RafLinkType.Recruit, accountInfo.gameAccountInfo.canSummon, accountInfo.isFavorite, accountInfo.gameAccountInfo.isWowMobile;
+	end
+end
+
+BNGetFriendInfo = function(friendIndex)
+	local accountInfo = C_BattleNet.GetFriendAccountInfo(friendIndex);
+	return getDeprecatedAccountInfo(accountInfo);
+end
